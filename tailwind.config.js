1549c864a2fe16ae3c7fddc4596f2fbacfce955f
Fatema Moharam <moharamfatema@gmail.com>
2023-01-04 22:23:43 +0200

    * set up color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,26 @@ module.exports = {
 		'./src/**/*.{js,jsx,ts,tsx}',
 	],
 	theme: {
+		colors: {
+			// Primary
+			primaryBlack: '#242731',
+			primaryGrey: '#E0E4E7',
+			primaryPurple: '#A162F7',
+			// Secondary
+			secondaryPurple: '#A162F7',
+			secondaryBlue: '#2884FF',
+			secondaryYellow: '#F6CC0D',
+			secondaryRed: '#FF6370',
+			secondaryGreen: '#70CF97',
+			secondaryOrange: '#FF764C',
+			// Gray
+			gray100: '#E0E4E7',
+			gray200: '#A4A5A6',
+			gray300: '#7C7C8D',
+			gray400: '#72767C',
+			gray500: '#5F6165',
+			gray600: '#242731',
+		},
 		fontFamily: {
 			sans: ['DM Sans', 'sans-serif'],
 		},
