4fda65be66ef7891cbdf42d64218beacbc910365
Fatema Moharam <moharamfatema@gmail.com>
2023-01-07 20:56:30 +0200

    adjust booking dims

--- src/app/modules/booking/components/car/index.tsx
+++ src/app/modules/booking/components/car/index.tsx
@@ -10,13 +10,13 @@ interface IcarProps {
 const Car: FC<IcarProps> = ({ car }) => {
 	return (
 		<div
-			className='car bg-white min-w-[325px] min-h-[267px] rounded-2xl flex flex-col gap-[10px] p-6 text-left text-[#72767C]'
+			className='car bg-white min-w-[325px] max-h-[267px] rounded-2xl flex flex-col gap-[10px] p-6 text-left text-[#72767C]'
 			key={car.id}
 		>
 			<CarHeader car={car} />
 			<h6 className='car__model'>{car.model}</h6>
 			<div className='car__image m-auto'>
-				<img src={car.image} alt={car.name} />
+				<img src={car.image} alt={car.name} className='max-h-[115px]' />
 			</div>
 			<CarFooter car={car} />
 		</div>
